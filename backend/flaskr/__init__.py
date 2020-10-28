c72186fe4bab38e9dbafa2b294243cb5a9c878c6
Fahad Almutairi <f.s.almotery@gmail.com>
2020-10-28 06:20:08 +0300

    first commit

---
+++ backend/flaskr/__init__.py
@@ -0,0 +1,231 @@
+import os
+from flask import Flask, request, abort, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+import random
+
+from models import setup_db, Question, Category
+
+QUESTIONS_PER_PAGE = 10
+
+def paginate(request, selection):
+  page = request.args.get('page', 1, type=int)
+  start =  (page - 1) * QUESTIONS_PER_PAGE
+  end = start + QUESTIONS_PER_PAGE
+
+  questions = [question.format() for question in selection]
+  current_questions = questions[start:end]
+
+  return current_questions
+
+
+def create_app(test_config=None):
+
+  # create and configure the app
+  app = Flask(__name__)
+  setup_db(app)
+  CORS(app)
+
+  # CORS Headers 
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+  # endpoint for all available categories
+  @app.route('/categories')
+  def categories_index():
+    categories = Category.query.order_by(Category.id).all()
+    categories = {category.id: category.type for category in categories}
+
+    # example output 
+    # "categories": {
+    #     "1": "Science",
+    # }
+
+    if len(categories) == 0:
+      abort(404)
+
+    return jsonify({
+      'success': True,
+      'categories': categories,
+      'total_categories': len(categories)
+    })
+
+  # endpoint for questions with pagination (every 10 questions)
+  @app.route('/questions')
+  def questions_index():
+    selection = Question.query.order_by(Question.id).all()
+    categories = Category.query.order_by(Category.id).all()
+    current_questions = paginate(request, selection)
+    categories = {category.id: category.type for category in categories}
+    
+    if len(current_questions) == 0:
+      abort(404)
+
+    return jsonify({
+      'success': True,
+      'questions': current_questions,
+      'total_questions': len(Question.query.all()),
+      'categories': categories,
+      'current_category': None
+    })
+
+  # endpoint for DELETE a question by id (destroy) 
+  @app.route("/questions/<question_id>", methods=['DELETE'])
+  def question_destroy(question_id):
+    try:
+      data = Question.query.get(question_id)
+      data.delete()
+
+      return jsonify({
+          'success': True,
+          'deleted': question_id
+      })
+    except:
+      abort(422)
+
+  # endpoint for new question (store) OR search on question
+  @app.route('/questions', methods=['POST'])
+  def question_store_or_search():
+    body = request.get_json()
+
+    question = body.get('question', None)
+    answer = body.get('answer', None)
+    category = body.get('category', None)
+    difficulty = body.get('difficulty', None)
+    search = body.get('searchTerm', None)
+
+
+    if search:
+      data = Question.query.filter( Question.question.ilike(f'%{search}%') ).all()
+      questions = []
+      for d in data:
+        questions.append({
+          "id": d.id,
+          "question": d.question,
+          "answer": d.answer,
+          "category": d.category,
+          "difficulty": d.difficulty
+        })
+      return jsonify({
+          'success': True,
+          'questions': questions,
+          'total_questions': len(data),
+          'current_category': None
+      })
+
+    else: 
+      try:
+        data = Question(
+          question= question, 
+          answer= answer, 
+          category= category,
+          difficulty= difficulty
+        )
+        data.insert()
+
+        selection = Question.query.order_by(Question.id).all()
+        current_questions = paginate(request, selection)
+
+        return jsonify({
+          'success': True,
+          'created': data.id,
+          'questions': current_questions,
+          'total_questions': len(Question.query.all())
+        })
+        
+      except:
+            abort(422)
+
+  # endpoint to get questions based on category
+  @app.route("/categories/<category_id>/questions", methods=['GET'])
+  def questions_based_on_category(category_id):
+    try:
+
+      data = Question.query.filter( Question.category == category_id ).all()
+
+      questions = []
+      for d in data:
+        questions.append({
+          "id": d.id,
+          "question": d.question,
+          "answer": d.answer,
+          "category": d.category,
+          "difficulty": d.difficulty
+        })
+
+      return jsonify({
+          'success': True,
+          'questions': questions,
+          'total_questions': len(data),
+          'current_category': category_id
+      })
+
+    except:
+      abort(404)
+
+
+  # endpoint for play quizzes
+  @app.route('/quizzes', methods=['POST'])
+  def play_quiz():
+
+    try:
+      data = request.get_json()
+      quiz_category = data.get('quiz_category')
+      previous_questions = data.get('previous_questions')
+
+      # Choose Category "ALL" -> Get a random questions from all categories
+      if quiz_category['id'] == 0:
+        questions = Question.query.filter(Question.id.notin_(previous_questions)).all()
+
+      # Choose a specific category -> Get a random questions from this category
+      else:
+        questions = Question.query.filter_by(category=quiz_category['id']).filter(Question.id.notin_(previous_questions)).all()
+
+      questions_data = []
+      for question in questions:
+        questions_data.append({
+          "id": question.id,
+          "question": question.question,
+          "answer": question.answer,
+          "category": question.category,
+          "difficulty": question.difficulty
+        })
+      # print(questions_data)
+
+      # question
+      if len(questions_data) > 0:
+        question = random.choice(questions_data)
+      else:
+        question = None
+
+      return jsonify({
+          'success': True,
+          'question': question
+      })
+    except:
+          abort(422)
+
+
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+        "success": False, 
+        "error": 404,
+        "message": "resource not found"
+    }), 404
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+        "success": False, 
+        "error": 422,
+        "message": "unprocessable"
+    }), 422
+
+  
+  return app
+
+    
---
+++ backend/test_flaskr.py
@@ -0,0 +1,61 @@
+import os
+import unittest
+import json
+from flask_sqlalchemy import SQLAlchemy
+
+from flaskr import create_app
+from models import setup_db, Question, Category
+
+
+class TriviaTestCase(unittest.TestCase):
+    """This class represents the trivia test case"""
+
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "trivia_test"
+        self.database_path ="postgresql://{}:{}@{}/{}".format('postgres','root','localhost:5432', 'trivia_test')
+        setup_db(self.app, self.database_path)
+
+        self.new_question = {
+            'question': 'new question',
+            'answer': 'new answer',
+            'category': 1,
+            'difficulty': 5
+        }
+        
+        # binds the app to the current context
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all()
+    
+    def tearDown(self):
+        """Executed after reach test"""
+        pass
+
+    def test_get_paginated_questions(self):
+        res = self.client().get('/questions')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_questions'])
+        self.assertTrue(len(data['questions']))
+        
+    def test_404_sent_requesting_beyond_valid_page(self):
+        res = self.client().get('/questions?page=1000')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+       
+
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
